337547dd69c455b86d47a78d74094fbaa6ffd5cf
krstkaan <k.kaanakbulak@gmail.com>
2024-07-25 11:39:00 +0300

    MusicActivity created backg added.

--- app/src/main/java/com/sufaka/lullaby/MainActivity.kt
+++ app/src/main/java/com/sufaka/lullaby/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.sufaka.lullaby
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+import android.widget.Button
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val myButton: Button = findViewById(R.id.startLullobyBtn)
+        myButton.setOnClickListener {
+            val intent = Intent(this, MusicActivity::class.java)
+            startActivity(intent)
         }
     }
 }
